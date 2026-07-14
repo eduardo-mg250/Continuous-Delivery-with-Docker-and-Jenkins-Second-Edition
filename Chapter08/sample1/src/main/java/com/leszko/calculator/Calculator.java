199b665c5ee0f9027d51e8b80eaa45f799a47b6d
eduardo-mg250 <eduardo.martinezgab@gmail.com>
2026-07-14 01:56:52 -0400

    Update comment formatting in Calculator.java

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -4,7 +4,7 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 /** 
- *  Simple calculator service that provides arithmetic operations. 
+ *  Simple calculator service that provides arithmetic operations.
  */
 
 @Service
